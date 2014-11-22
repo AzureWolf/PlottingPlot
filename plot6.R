7426252a5514c48b4fff46df698ce2f78d33fbb0
AzureWolf <AzureWolf@JPro.home>
2014-11-22 16:49:21 -0500

    Answer question 6 of project

--- plot6.R
+++ plot6.R
@@ -2,3 +2,27 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
+library(ggplot2)
+getAnnualPollutant <- function(NEI, yearWanted) {
+	sum(subset(NEI, year == yearWanted)$Emissions)
+}
+
+getSources <- function(SCC, sourceWanted) {
+	subset(SCC, grepl(sourceWanted, EI.Sector))$SCC
+}
+
+coalCombustionSources <- getSources(SCC, "On-Road.*Vehicles")
+processedTable <- data.frame()
+baltimore <- subset(NEI, fips == "24510" & type == "ON-ROAD")
+losAngeles <- subset(NEI, fips == "06037" & type == "ON-ROAD")
+
+for(eachYear in unique(baltimore$year)) {
+	processedTable <- rbind( processedTable, cbind(eachYear, getAnnualPollutant(subset(baltimore, SCC %in% coalCombustionSources), eachYear)) )
+}
+
+for(eachYear in unique(baltimore$year)) {
+	processedTable <- rbind( processedTable, cbind(eachYear, getAnnualPollutant(subset(baltimore, SCC %in% coalCombustionSources), eachYear)) )
+}
+
+colnames(processedTable) <- c("year", "totalPollutant")
+qplot()
