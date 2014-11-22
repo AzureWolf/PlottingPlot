54d4eb3b4bee3228f40b3a36bb5833ddfccc0f3f
AzureWolf <AzureWolf@JPro.home>
2014-11-22 15:52:18 -0500

    Answer question 3 of project

--- plot3.R
+++ plot3.R
@@ -2,3 +2,19 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
+library(ggplot2)
+getAnnualPollutant <- function(NEI, yearWanted) {
+	sum(subset(NEI, year == yearWanted)$Emissions)
+}
+
+processedTable <- data.frame()
+baltimore <- subset(NEI, fips == "24510")
+
+for(eachYear in unique(baltimore$year)){
+	for(eachType in unique(baltimore$type)) {
+		processedTable <- rbind( processedTable, cbind(eachYear, eachType, getAnnualPollutant(subset(baltimore, type == eachType), eachYear)) )
+	}
+}
+
+colnames(processedTable) <- c("year", "type", "totalPollutant")
+qplot(year, as.numeric(as.character(totalPollutant)), data = processedTable, color = type, group = type, geom = "smooth")
