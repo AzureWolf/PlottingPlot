07cb74f82ba7515b10f4e50b6a6408d44114c24d
AzureWolf <AzureWolf@JPro.home>
2014-11-22 16:34:42 -0500

    Fix conditional for subsetting data for step 4

--- plot4.R
+++ plot4.R
@@ -2,3 +2,21 @@
 NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 
+library(ggplot2)
+getAnnualPollutant <- function(NEI, yearWanted) {
+	sum(subset(NEI, year == yearWanted)$Emissions)
+}
+
+getSources <- function(SCC, sourceWanted) {
+	subset(SCC, grepl(sourceWanted, EI.Sector))$SCC
+}
+
+coalCombustionSources <- getSources(SCC, "Fuel Comb.*Coal")
+processedTable <- data.frame()
+
+for(eachYear in unique(NEI$year)) {
+	processedTable <- rbind( processedTable, cbind(eachYear, getAnnualPollutant(subset(NEI, SCC == coalCombustionSources), eachYear)) )
+}
+
+colnames(processedTable) <- c("year", "totalPollutant")
+plot(processedTable[,1], processedTable[,2], type="l")
